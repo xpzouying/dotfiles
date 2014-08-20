a526eccb159cde7a543bb180eddfdd4e67d98ebc
Zou, Ying <xpzouying@gmail.com>
2014-08-20 12:02:16 +0800

    Change install.sh to adaptor any path.
    Change: modify path to $(pwd)

--- install.sh
+++ install.sh
@@ -7,13 +7,14 @@
 
 ########## Variables
 
-dir_files=~/dotfiles    # dotfiles directory
-files="vimrc vimrc.bundles gitconfig"   # list of files in $dir_files
+# dir_files=~/dotfiles    # dotfiles directory
+dotfiles_path=$(pwd)
+files="vimrc vimrc.bundles gitconfig"   # list of files in $dotfiles_path
 
 ########## Script
-echo "Making files from $dir_files to $HOME"
+echo "Making files from $dotfiles_path to $HOME"
 
 for file in $files; do
     echo "Creating link to $file."
-    ln -sf $dir_files/$file	~/.$file
+    ln -sf $dotfiles_path/$file ~/.$file
 done
