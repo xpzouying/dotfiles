e0f8398c3a93847884afa755c281adb0d52525d5
Ying Zou <xpzouying@gmail.com>
2017-06-06 01:05:18 +0800

    Add install_zsh.sh.

---
+++ neovim_config/utils/zshrc_conf/install_zsh.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+
+
+PLATFORM='unknown'
+UNAME=`uname`
+
+if [[ "$UNAME" == 'Linux' ]]; then
+    PLATFORM='linux'
+fi
+
+
+echo "OS Platform is" $PLATFORM
+
+
+echo "Install zplug"
+if [[ "$PLATFORM" == 'linux' ]]; then
+    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
+fi
+
+
+echo "Link zsh"
+ln -s "$(pwd)/.zshrc" "$HOME/.zshrc"
+
+
+echo "Install oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
