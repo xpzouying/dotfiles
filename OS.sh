d1b575a9e2c1454f47ed72477e152fbe42fc90f0
Ying Zou <xpzouying@gmail.com>
2017-08-29 17:43:08 +0800

    change zsh to fish shell for speed up.

---
+++ OS.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+
+PLATFORM='unknown'
+UNAMESTR=$(uname)
+if [[ "$UNAMESTR" == 'Linux' ]]; then
+    PLATFORM='LINUX'
+elif [[ "$UNAMESTR" == 'FreeBSD' ]]; then
+    PLATFORM='FREEBSD'
+elif [[ "$UNAMESTR" == 'Darwin' ]]; then
+    PLATFORM='MACOSX'
+fi
+
+
+# use it
+# if [[ "$PLATFORM" == 'MACOSX' ]]; then
+#     brew install git-flow-avh
+#     brew install git bash-completion  # for bash-completion
+# fi
