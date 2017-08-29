39a07c4c617f50444aba1e8166ef34545f22de7a
Ying Zou <xpzouying@gmail.com>
2017-08-29 18:03:16 +0800

    add config for fish shell.

---
+++ fish_shell/config.fish
@@ -0,0 +1,20 @@
+
+# brew-bottles
+set -x HOMEBREW_BOTTLE_DOMAIN https://mirrors.ustc.edu.cn/homebrew-bottles
+
+
+# golang
+set -x GOPATH $HOME/src/GOPATH
+set -x PATH $PATH /usr/local/go/bin $GOPATH/bin/
+
+# python3 default
+alias python python3
+alias pip pip3
+
+#Add anaconda & anaconda3
+set -x PATH $PATH $HOME/anaconda/bin/
+set -x PATH $HOME/anaconda3/bin $PATH
+
+# 256 color
+set -x TERM xterm-256color
+
