ea156025e7db65a3e7e1715bb8a3581746d691a9
zy <xpzouying@gmail.com>
2020-10-27 16:15:37 +0000

    update oh-my-zsh script url

--- zshrc_conf/install_zsh.sh
+++ zshrc_conf/install_zsh.sh
@@ -31,7 +31,8 @@ touch "$HOME/.local_rc"
 
 
 echo "Install oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 
 
 echo "Install zplus"
