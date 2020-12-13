ce9dab5c61145eda18a7521dd1e87b9ff41f7e99
zy <xpzouying@gmail.com>
2020-12-14 00:57:26 +0800

    vim-config: new vim config
    from: https://gist.github.com/nf/d89c8ba5db9bbf23da18
    Author: Andrew Gerrand

---
+++ vim_config/README.md
@@ -0,0 +1,3 @@
+# vim config
+
+Ref: [Andrew Gerrand Gist](https://gist.github.com/nf/d89c8ba5db9bbf23da18)
---
+++ vim_config/vim-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
+
+
+echo '
+execute pathogen#infect()
+let g:go_fmt_command = "goimports"
+set autoindent
+filetype plugin indent on
+set dir=/tmp
+set backupdir=/tmp
+' > ~/.vimrc
