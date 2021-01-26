995238feaa90c46e67a9febbd15694ed2fcb523d
zy <xpzouying@gmail.com>
2021-01-26 13:29:18 +0800

    update: zsh install script
    1. update zshrc root path: user $HOME
    2. add p10k install cmd

--- zshrc_conf/install_zsh.sh
+++ zshrc_conf/install_zsh.sh
@@ -41,6 +41,8 @@ mkdir $HOME/zybin
 echo "Install zplus"
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
+# powerlevel10k
+git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # --- disalbe conda auto activating
 # Ref: https://stackoverflow.com/questions/54429210/how-do-i-prevent-conda-from-activating-the-base-environment-by-default
