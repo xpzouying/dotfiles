ef6d6d1968f5946dfa7bf630384a66de2c3c3796
zy <xpzouying@gmail.com>
2021-02-06 00:19:23 +0800

    update brew for dev_setup.sh

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 
+
+echo "install brew for macos"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> $HOME/.zprofile
+eval $(/opt/homebrew/bin/brew shellenv)
+
+
+
 echo "setting pip"
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+
