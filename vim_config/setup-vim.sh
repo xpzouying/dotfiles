b63440a4e85faa34387ac9462a788899fb77c163
zy <xpzouying@gmail.com>
2021-12-28 01:00:01 +0800

    update vim

--- vim_config/setup-vim.sh
+++ vim_config/setup-vim.sh
@@ -10,16 +10,26 @@ cat > ~/.vimrc <<EOL
 
 set nocompatible
 set autoread
-set encoding=utf-8
+set encoding=utf-8 nobomb
 set incsearch
 set hlsearch
+set expandtab
+set shiftwidth=4
+set softtabstop=4
+set autoindent
+set smartindent
+
+set clipboard=unnamed
+
+let mapleader=","
 
 set dir=/tmp
 set backupdir=/tmp
 
 call plug#begin('~/.vim/plugged')
-" Plug 'fatih/vim-go', { 'do': ':GoUpdateBinaries' }
-Plug 'govim/govim'
+Plug 'fatih/vim-go', { 'do': ':GoUpdateBinaries' }
+" Plug 'govim/govim'
+Plug 'neoclide/coc.nvim', {'branch': 'release'}
 Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
 Plug 'junegunn/fzf.vim'
 
@@ -28,8 +38,8 @@ call plug#end()
 map <C-p> :Files<CR>
 
 " let g:go_fmt_command = "goimports"
-let g:go_fmt_command = "gopls"
-let g:go_info_mode = 'gopls'
-let g:go_def_mode = 'gopls'
+" let g:go_fmt_command = "gopls"
+" let g:go_info_mode = 'gopls'
+" let g:go_def_mode = 'gopls'
 
 EOL
